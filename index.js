90e57bd4ad7b3056926b3ebe129022d382f0a2a4
Samruddhi-30 <157288103+Samruddhi-30@users.noreply.github.com>
2024-07-15 22:14:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs"); 
+});
+
+app.get("/about",(req,res) =>{
+    res.render("about.ejs");
+});
+
+app.get("/contact",(req,res) =>{
+    res.render("contact.ejs");
+});
+
+app.get("/blog",(req,res) =>{
+    res.render("blog.ejs");
+});
+
+
+
+app.listen(port,() =>{
+    console.log("Listening on port ",port);
+
+});
+
+
+ 
